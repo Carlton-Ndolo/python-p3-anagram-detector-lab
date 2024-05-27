a4ae336cf3ac5da22126a1824bbb95dbd5cfed21
Carlton-Ndolo <carltonmutua80@gmail.com>
2024-05-27 12:11:57 +0300

    Complete Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = [w for w in words if self.is_anagram(w)]
+        return anagrams if anagrams else []
+
+    def is_anagram(self, other):
+        return sorted(self.word.lower()) == sorted(other.lower())
+
+
